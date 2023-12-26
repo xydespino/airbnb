797410ed4934f7289f7183755d8c6fbcc9d57c5d
Xyd Espino <xydespino@outlook.com>
2023-12-26 13:33:37 -0400

    Added Login Page

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -5,7 +5,7 @@ export default function Header(){
         <div>
             <header className='flex justify-between'>
             {/* Logo Top Right */}
-            <a href="" className="flex items-center gap-1">
+            <a href="/" className="flex items-center gap-1">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 -rotate90">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
                 </svg>
